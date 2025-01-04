b2e18bad90c0c0d18012aa11cc319a8ca1747c13
ngenohkevin <ngenohkevin19@gmail.com>
2025-01-04 21:47:31 +0300

    install tailwindcss

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/ngenohkevin/ngenohkev/internals/components"
+)
+
 func main() {
-	
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	//create a new router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		err := components.Home("NgenohKev Blog").Render(r.Context(), w)
+		if err != nil {
+			logger.Error("An error occurred while rendering the home page", err)
+		}
+	})
+
+	router.HandleFunc("/header", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	//start the server
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	logger.Info("Server is running on port 8080")
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		logger.Error("An error occurred while starting the server", err)
+	}
 }
